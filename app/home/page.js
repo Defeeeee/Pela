23628e75e1fe753e2dbd11a4c1d00489ccc7b253
Matías Grynfeld <matiasgrynfeld@gmail.com>
2026-04-10 14:13:46 -0300

    fix: randomness in loading delay

--- app/home/page.js
+++ app/home/page.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import LoadingScreen from '../LoadingScreen';
+import sharp from 'sharp';
+import { start } from 'repl';
 
-export default function Home() {
+export async function Home() {
   const IMGS_DIR = path.join(process.cwd(), 'public', 'imgs');
   const files = fs.readdirSync(IMGS_DIR).filter(f => /\.(jpe?g|png|gif|webp)$/i.test(f));
 
@@ -15,8 +17,38 @@ export default function Home() {
   }
 
   const fileName = files[Math.floor(Math.random() * files.length)];
-  const delay = Math.floor(Math.random() * 2001) + 2000; // 2000-4000ms
-  const title = fileName.replace(/\.[^/.]+$/, '').replace(/\b\w/g, c => c.toUpperCase());
 
-  return <LoadingScreen fileName={fileName} delay={delay} initialTitle={title} />;
+  const stats = await getImageData(path.join(IMGS_DIR, fileName));
+
+  let extraDelay = Math.floor(Math.random() * 4001) - 2000; // -2000ms to +2000ms
+
+  const title = fileName.replace(/\.[^/.]+$/, '').replace(/\b\w/g, c => c.toUpperCase());
+  
+  const maxDelayFromImage = Math.floor(Math.random() * 4001); + 4000 // 0 to 2000ms
+
+  if (stats.height*stats.width > maxDelayFromImage) {
+    extraDelay -= stats.height * stats.width - maxDelayFromImage;
+  }
+
+  console.log(stats.height*stats.width+extraDelay)
+  return <LoadingScreen fileName={fileName} delay={stats.height * stats.width + extraDelay} initialTitle={title} />;
+}
+
+export async function getImageData(path) {
+  const imageBuffer = fs.readFileSync(path);
+
+  const image = sharp(imageBuffer);
+  
+  const metadata = await image.metadata();
+  
+  const { data, info } = await image
+    .raw()
+    .toBuffer({ resolveWithObject: true });
+
+  return {
+    width: metadata.width,
+    height: metadata.height,
+    pixelArray: data,
+    channels: info.channels
+  };
 }
