5532ee97090fd25f67b63bae2be472bfa0c3f922
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2026-05-05 11:48:56 +0000

    feat: add closed page with consistent aesthetics and pelado qepd

--- app/customPages/pages.js
+++ app/customPages/pages.js
@@ -1,11 +1,13 @@
 import HealthPage from '../health/page';
 import HomePage from '../home/page';
 import InstantPage from '../instant/page';
+import ClosedPage from '../closed/page';
 
 const pages = {
   '/': HomePage,
   '/health': HealthPage,
   '/instant': InstantPage,
+  '/closed': ClosedPage,
 };
 
 export default pages;
